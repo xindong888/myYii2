79384b301e35e65c09898e92a8c056bfbc915eb6
金鹰视频教程 <xindong888@qq.com>
2017-10-21 09:04:58 -0500

    删除测试会更好

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require(__DIR__ . '/vendor/autoload.php');
 require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
 
 require (__DIR__.'/common/config/bootstrap.php');
-echo "cc";
+
 $config = require(__DIR__ . '/home/config/web.php');
 $app = new \yii\web\Application($config);
 $app->run();
