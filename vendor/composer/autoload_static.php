319f8e40aa13c5e0309f988fa1a44586651211ae
xindong888 <xindong888@163.com>
2017-10-22 01:58:43 +0000

    添加bootstrap

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -14,6 +14,7 @@ class ComposerStaticInit999339993fb2c413a5fde7ffb3d1f246
         'y' => 
         array (
             'yii\\composer\\' => 13,
+            'yii\\bootstrap\\' => 14,
             'yii\\' => 4,
         ),
         'c' => 
@@ -26,6 +27,10 @@ class ComposerStaticInit999339993fb2c413a5fde7ffb3d1f246
         'yii\\composer\\' => 
         array (
             0 => __DIR__ . '/..' . '/yiisoft/yii2-composer',
+        ),
+        'yii\\bootstrap\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/yiisoft/yii2-bootstrap',
         ),
         'yii\\' => 
         array (
